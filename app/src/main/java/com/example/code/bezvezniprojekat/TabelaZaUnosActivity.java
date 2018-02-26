4a33de110cc9842f6f9c0ae2c90d908045e9aa53
miljannn <vujsicmiljan00@gmail.com>
2018-02-26 20:42:15 +0100

    dodao 2 imageviewa u xml

--- app/src/main/java/com/example/code/bezvezniprojekat/TabelaZaUnosActivity.java
+++ app/src/main/java/com/example/code/bezvezniprojekat/TabelaZaUnosActivity.java
@@ -2,9 +2,10 @@ package com.example.code.bezvezniprojekat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class TabelaZaUnosActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
