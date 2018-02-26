59892307fdcf45feed1b19ecc533e9059ff25703
miljannn <vujsicmiljan00@gmail.com>
2018-02-26 20:36:14 +0100

    dodao TabelaUnos activity

---
+++ app/src/main/java/com/example/code/bezvezniprojekat/TabelaZaUnosActivity.java
@@ -0,0 +1,13 @@
+package com.example.code.bezvezniprojekat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabelaZaUnosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabela_za_unos);
+    }
+}
