44ea9e843b2753bd3a03aa46436b7f7f656b34ba
Aleksandar Markovic <aleksamarkoni@gmail.com>
2018-02-26 20:45:00 +0100

    Add a code inside activity

--- app/src/main/java/com/example/code/bezvezniprojekat/AnketaActivity.java
+++ app/src/main/java/com/example/code/bezvezniprojekat/AnketaActivity.java
@@ -2,6 +2,8 @@ package com.example.code.bezvezniprojekat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class AnketaActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class AnketaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anketa);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.dobar:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.los:
+                if (checked)
+                    // Ninjas rule
+                    break;
+            case R.id.zao:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.debeo:
+                if (checked)
+                    // Ninjas rule
+                    break;
+            case R.id.ruzan:
+                if (checked)
+                    // Pirates are the best
+                    break;
+        }
     }
 }
